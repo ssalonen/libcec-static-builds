b96f17e6a387ec68850fd75b2ded1a1fc4e9b93b
Sami Salonen <ssalonen@gmail.com>
2024-12-16 20:25:43 +0200

    commit .github actions

---
+++ .github/actions/build-static-libcec-unix/build.sh
@@ -0,0 +1,36 @@
+#! /usr/bin/env bash
+
+set -euxo pipefail
+
+path="$1"
+build_type="$2"
+
+case "$build_type" in
+  'debug')
+    cmake_build_type='Debug'
+    ;;
+
+  'release')
+    cmake_build_type='RelWithDebInfo'
+    ;;
+
+  *)
+    echo "invalid build type"
+    exit 1
+    ;;
+esac
+
+# from https://stackoverflow.com/questions/28678505/add-command-arguments-using-inline-if-statement-in-bash
+args=()
+[[ "$HAVE_LINUX_API" == "true" ]] && args+=( '-DHAVE_LINUX_API=1' )
+args+=( "-DHAVE_P8_USB=1" "-DHAVE_P8_USB_DETECT=1" "-D" "CMAKE_BUILD_TYPE=$cmake_build_type" "-D" "BUILD_STATIC_LIB=True" "-D" "CMAKE_CXX_STANDARD=11" "-Wno-dev" )
+echo "${args[@]}"
+
+cd "$path"
+cmake -S . -B build "${args[@]}"
+cmake --build build
+
+mkdir -p dist/include
+ls -R build
+find build \( -name '*.a' -o -name '*.so' -o -name '*.dylib' \) -print -exec cp {} dist \;
+find include -name '*.h' -print -exec cp --parents {} dist \;
