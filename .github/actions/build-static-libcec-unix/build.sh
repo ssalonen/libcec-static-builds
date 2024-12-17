0f9953e584b2947eee82b269571134362901204a
Sami Salonen <ssalonen@gmail.com>
2024-12-17 07:42:52 +0200

    more debug with cmake

--- .github/actions/build-static-libcec-unix/build.sh
+++ .github/actions/build-static-libcec-unix/build.sh
@@ -22,22 +22,24 @@ esac
 
 # from https://stackoverflow.com/questions/28678505/add-command-arguments-using-inline-if-statement-in-bash
 args=()
-[[ "$HAVE_LINUX_API" == "true" ]] && args+=( '-DHAVE_LINUX_API=1' )
+[[ "$HAVE_LINUX_API" == "true" ]] && args+=( '-DHAVE_LINUX_API=1" )
 args+=( "-DHAVE_P8_USB=1" "-DHAVE_P8_USB_DETECT=1" "-D" "CMAKE_BUILD_TYPE=$cmake_build_type" "-D" "BUILD_STATIC_LIB=True" "-D" "CMAKE_CXX_STANDARD=11" "-Wno-dev" )
 echo "${args[@]}"
 
 cd "$path"
 # generate "build system" and then build
+
 # first for platform
 mkdir platform_build
 PLATFORMBUILD=$(readlink -f platform_build)
-cmake -S src/platform -B platform_build "${args[@]}"
+cmake -S src/platform -B platform_build
 env "p8-platform_ROOT=$PLATFORMBUILD" cmake --build platform_build
-# same for all
-env "p8-platform_ROOT=$PLATFORMBUILD" "p8-platform_DIR=$PLATFORMBUILD/build" "p8-platform_INCLUDE_DIRS=$PLATFORMBUILD/include" "p8-platform_LIBRARY=$PLATFORMBUILD/build/libp8-platform.a" cmake -S . -B build "${args[@]}"
-env "p8-platform_ROOT=$PLATFORMBUILD" "p8-platform_DIR=$PLATFORMBUILD/build" "p8-platform_INCLUDE_DIRS=$PLATFORMBUILD/include" "p8-platform_LIBRARY=$PLATFORMBUILD/build/libp8-platform.a" cmake --build build
+
+# same for libcec
+env "p8-platform_ROOT=$PLATFORMBUILD" "p8-platform_DIR=$PLATFORMBUILD/build" "p8-platform_INCLUDE_DIRS=$PLATFORMBUILD/include" "p8-platform_LIBRARY=$PLATFORMBUILD/build/libp8-platform.a" cmake --debug-output -S src/libcec -B build -DCMAKE_VERBOSE_MAKEFILE=ON "${args[@]}"
+env "p8-platform_ROOT=$PLATFORMBUILD" "p8-platform_DIR=$PLATFORMBUILD/build" "p8-platform_INCLUDE_DIRS=$PLATFORMBUILD/include" "p8-platform_LIBRARY=$PLATFORMBUILD/build/libp8-platform.a" cmake --debug-output -DCMAKE_VERBOSE_MAKEFILE=ON --build build
 
 mkdir -p dist/include
 ls -R build
-find build \( -name '*.a' -o -name '*.so' -o -name '*.dylib' \) -print -exec cp {} dist \;
-find include -name '*.h' -print -exec cp --parents {} dist \;
+find build \( -name '*.a" -o -name '*.so" -o -name '*.dylib" \) -print -exec cp {} dist \;
+find include -name '*.h" -print -exec cp --parents {} dist \;
