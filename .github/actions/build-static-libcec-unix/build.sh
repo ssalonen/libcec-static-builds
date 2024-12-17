95d67b0c0471d9438b3d151b61e2f2707f623601
Sami Salonen <ssalonen@gmail.com>
2024-12-17 08:55:20 +0200

    install dir

--- .github/actions/build-static-libcec-unix/build.sh
+++ .github/actions/build-static-libcec-unix/build.sh
@@ -22,7 +22,7 @@ esac
 
 # from https://stackoverflow.com/questions/28678505/add-command-arguments-using-inline-if-statement-in-bash
 args=()
-[[ "$HAVE_LINUX_API" == "true" ]] && args+=( '-DHAVE_LINUX_API=1' )
+[[ "$HAVE_LINUX_API" = "true" ]] && args+=( '-DHAVE_LINUX_API=1' )
 args+=( "-DHAVE_P8_USB=1" "-DHAVE_P8_USB_DETECT=1" "-D" "CMAKE_BUILD_TYPE=$cmake_build_type" "-D" "BUILD_STATIC_LIB=True" "-D" "CMAKE_CXX_STANDARD=11" "-Wno-dev" )
 echo "${args[@]}"
 
@@ -32,7 +32,7 @@ cd "$path"
 # first for platform
 mkdir platform_build
 PLATFORMBUILD=$(readlink -f platform_build)
-cmake -S src/platform -B platform_build
+cmake -DCMAKE_INSTALL_PREFIX=platform_build -S src/platform -B platform_build
 # Also install
 env "p8-platform_ROOT=$PLATFORMBUILD" cmake --build platform_build --target install
 
@@ -42,7 +42,7 @@ cmake \
  -D"p8-platform_DIR=$PLATFORMBUILD/build" \
  -D"p8-platform_INCLUDE_DIRS=$PLATFORMBUILD/include" \
  -D"p8-platform_LIBRARY=$PLATFORMBUILD/build/libp8-platform.a" \
- -DCMAKE_VERBOSE_MAKEFILE=ON "${args[@]}" \
+ -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_INSTALL_PREFIX=build "${args[@]}" \
  -S . -B build
 
 
@@ -54,7 +54,7 @@ ls -R src/platform
 find . -type d -name p8-platform
 find . -type f -name os.h
 
-env "p8-platform_ROOT=$PLATFORMBUILD" cmake --build build
+env "p8-platform_ROOT=$PLATFORMBUILD" cmake --build build --target install
 
 find build \( -name '*.a' -o -name '*.so' -o -name '*.dylib' \) -print -exec cp {} dist \;
 find include -name '*.h' -print -exec cp --parents {} dist \;
